9d7100767ad50dbe4f55a22dc555f9d3c555b7cc
Gabriel Resende <gabriellrr2005@gmail.com>
2023-06-27 15:29:51 -0300

    Consulta simples com query methods

--- src/main/java/com/resendegabriel/mongo/resources/PostResource.java
+++ src/main/java/com/resendegabriel/mongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.resendegabriel.mongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.resendegabriel.mongo.domain.Post;
+import com.resendegabriel.mongo.resources.util.URL;
 import com.resendegabriel.mongo.services.PostService;
 
 @RestController
@@ -22,4 +26,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findbyTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
 }
