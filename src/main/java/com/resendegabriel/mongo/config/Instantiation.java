9173a44012addae3a8e6889441a3434daef20091
Gabriel Resende <gabriellrr2005@gmail.com>
2023-06-27 13:23:55 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/resendegabriel/mongo/config/Instantiation.java
+++ src/main/java/com/resendegabriel/mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.resendegabriel.mongo.domain.Post;
 import com.resendegabriel.mongo.domain.User;
+import com.resendegabriel.mongo.dto.AuthorDTO;
 import com.resendegabriel.mongo.repository.PostRepository;
 import com.resendegabriel.mongo.repository.UserRepository;
 
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraço!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraço!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
