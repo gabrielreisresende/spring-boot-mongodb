17bdc4b6a28c995195032381470dd0428ad5355b
Gabriel Resende <gabriellrr2005@gmail.com>
2023-06-27 13:45:12 -0300

    Obtendo post por id

---
+++ src/main/java/com/resendegabriel/mongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.resendegabriel.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.resendegabriel.mongo.domain.Post;
+import com.resendegabriel.mongo.repository.PostRepository;
+import com.resendegabriel.mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
