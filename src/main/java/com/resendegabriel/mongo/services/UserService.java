74407dbd3d77b33596aedef4c2d7ed5eea466e66
Gabriel Resende <gabriellrr2005@gmail.com>
2023-06-27 11:29:04 -0300

    Obtendo um usuario por id

--- src/main/java/com/resendegabriel/mongo/services/UserService.java
+++ src/main/java/com/resendegabriel/mongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.resendegabriel.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.resendegabriel.mongo.domain.User;
 import com.resendegabriel.mongo.repository.UserRepository;
+import com.resendegabriel.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
